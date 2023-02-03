56e147bc90b8bef93b0835968b03dbabd6ef3f54
Sashkoooo <o.denyseko@hotmail.com>
2023-02-03 16:02:47 +0200

    Solution v1.2

--- app/main.py
+++ app/main.py
@@ -64,7 +64,8 @@ class Slide:
     def __init__(
             self,
             name: str,
-            limitation_class: SlideLimitationValidator
+            limitation_class: type[SlideLimitationValidator]
+
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
