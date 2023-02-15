288ff0aa5afc329aec83d66eaa0d2d6357af473f
kuznetsb <kuznetsbog@gmail.com>
2023-02-15 09:31:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,25 +1,71 @@
 from abc import ABC
+from typing import Any
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: Any, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: Any, owner: Any) -> int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: Any, value: int) -> None:
+        if value not in range(self.min_amount, self.max_amount + 1):
+            return
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    weight = IntegerRange(20, 50)
+    height = IntegerRange(80, 120)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    weight = IntegerRange(50, 120)
+    height = IntegerRange(120, 220)
 
 
 class Slide:
-    pass
+    def __init__(
+            self,
+            name: str,
+            limitation_class: SlideLimitationValidator
+    ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        if isinstance(self.limitation_class, AdultSlideLimitationValidator):
+            self.limitation_class = AdultSlideLimitationValidator
+        elif isinstance(self.limitation_class,
+                        ChildrenSlideLimitationValidator):
+            self.limitation_class = ChildrenSlideLimitationValidator
+        permission = self.limitation_class(visitor.age,
+                                           visitor.weight,
+                                           visitor.height)
+        if all([hasattr(permission, "age"),
+                hasattr(permission, "weight"),
+                hasattr(permission, "height")]):
+            return True
+        return False
