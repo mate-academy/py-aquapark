d6bce9f2143f45b8a9beee82fc3ab92479f879ae
Roman28101 <r.kovalenko28101@gmail.com>
2023-02-15 17:37:48 +0800

    Solution add typing

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC
+from typing import Type
 
 
 class IntegerRange:
@@ -57,7 +58,7 @@ class Slide:
     def __init__(
             self,
             name: str,
-            limitation_class: type(SlideLimitationValidator)
+            limitation_class: Type[SlideLimitationValidator]
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
