602666ae766606e7e0b68fb276b7a2f309cd6ff4
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-01 17:35:29 +0300

    init

--- app/main.py
+++ app/main.py
@@ -2,7 +2,21 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount, max_amount):
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __get__(self, instance, owner):
+        return instance
+
+    def __set__(self, instance, value):
+        return instance
+
+    def __set_name__(self, owner, name):
+        return name
+
+
+
 
 
 class Visitor:
