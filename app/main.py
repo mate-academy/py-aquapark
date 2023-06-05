6b74965e92122fa2ea90195a59b134581379c8d7
Vadym <seredavadimsv@gmail.com>
2023-06-05 11:19:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
 from abc import ABC
+from typing import Type
+
+
+class SlideLimitationValidator(ABC):
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class IntegerRange:
@@ -6,14 +14,26 @@ class IntegerRange:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
-    def __set_name__(self, owner: object, name: str) -> None:
+    def __set_name__(
+            self,
+            owner: Type[SlideLimitationValidator],
+            name: str
+    ) -> None:
         self.private_name = "_" + name
 
-    def __get__(self, instance: object, owner: object) -> int:
+    def __get__(
+            self,
+            instance: Type[SlideLimitationValidator],
+            owner: Type[SlideLimitationValidator]
+    ) -> int:
         value = getattr(instance, self.private_name)
         return value
 
-    def __set__(self, instance: object, value: int) -> None:
+    def __set__(
+            self,
+            instance: Type[SlideLimitationValidator],
+            value: int
+    ) -> None:
         if not isinstance(value, int | float):
             raise TypeError("Quantity should be int or float.")
 
@@ -27,13 +47,6 @@ class IntegerRange:
 class Visitor:
     def __init__(self, name: str, age: int, weight: int, height: int) -> None:
         self.name = name
-        self.age = age
-        self.weight = weight
-        self.height = height
-
-
-class SlideLimitationValidator(ABC):
-    def __init__(self, age: int, weight: int, height: int) -> None:
         self.age = age
         self.weight = weight
         self.height = height
@@ -55,7 +68,7 @@ class Slide:
     def __init__(
             self,
             name: str,
-            limitation_class: type[SlideLimitationValidator]
+            limitation_class: Type[SlideLimitationValidator]
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
