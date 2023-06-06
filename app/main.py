a075cc8c54e827e1b3705b497a711934d5db2aae
Olexandr <maxbovling23432@gmail.com>
2023-06-06 23:43:54 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -7,49 +7,36 @@ class IntegerRange:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
+    def __get__(self, instance: object = None, owner: Type = None) -> None:
+        if instance is None:
+            return self
+        return getattr(instance, self.name)
+
+    def __set__(self, instance: object, value: int) -> None:
+        if not isinstance(value, int):
+            raise ValueError("Value must be an integer.")
+        if not self.min_amount <= value <= self.max_amount:
+            raise (
+                ValueError(f"Value must be between {self.min_amount} "
+                           f"and {self.max_amount}.")
+            )
+        setattr(instance, self.name, value)
+
     def __set_name__(self, owner: Type, name: str) -> None:
         self.name = name
 
-    def __get__(self, instance: object = None, owner: Type = None) -> bool:
-        return self.value
-
-    def __set__(self, instance: object, value: bool = True) -> None:
-        self.value = value
-        limitation_ranges = {
-            ChildrenSlideLimitationValidator: ChildrenSlideLimitationValidator.
-            children_slide_limitation_validator,
-            AdultSlideLimitationValidator: AdultSlideLimitationValidator.
-            adult_slide_limitation_validator,
-        }
-
-        limitation_range = limitation_ranges.get(self.limitation_class)
-
-        if (
-            not (limitation_range["age"][0] <= self.age
-                 <= limitation_range["age"][1])
-            or not (limitation_range["height"][0] <= self.height
-                    <= limitation_range["height"][1])
-            or not (limitation_range["weight"][0] <= self.weight
-                    <= limitation_range["weight"][1])
-        ):
-            self.value = False
-        else:
-            self.value = True
-
 
 class Visitor:
-    def __init__(
-            self, age: int, height: int, weight: int,
-            name: str = ""
-    ) -> None:
+    def __init__(self, name: str, age: int, height: int, weight: int) -> None:
+        self.name = name
         self.age = age
         self.height = height
         self.weight = weight
-        self.name = name
 
 
 class SlideLimitationValidator(ABC):
-    def __init__(self, age: int, height: int, weight: int) -> None:
+    def __init__(self, name: str, age: int, height: int, weight: int) -> None:
+        self.name = name
         self.age = age
         self.height = height
         self.weight = weight
@@ -61,6 +48,9 @@ class ChildrenSlideLimitationValidator(SlideLimitationValidator):
         "height": (80, 120),
         "weight": (20, 50)
     }
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
@@ -69,16 +59,33 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
         "height": (120, 220),
         "weight": (50, 120)
     }
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
-class Slide:
-    def __init__(self, name: str, limitation_class: int) -> None:
+class Slide(SlideLimitationValidator):
+    def __init__(
+            self,
+            name: str,
+            limitation_class: Type[SlideLimitationValidator]
+    ) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
-    def can_access(self, visitor: Visitor, value: bool = True) -> bool:
-        self.age = visitor.age
-        self.height = visitor.height
-        self.weight = visitor.weight
-        IntegerRange.__set__(self, value)
-        return IntegerRange.__get__(self)
+    def can_access(self, visitor: Visitor) -> bool:
+        age_range = self.limitation_class.age
+        height_range = self.limitation_class.height
+        weight_range = self.limitation_class.weight
+
+        if (
+            age_range.min_amount <= visitor.age
+                <= age_range.max_amount
+                and height_range.min_amount <= visitor.height
+                <= height_range.max_amount
+                and weight_range.min_amount <= visitor.weight
+                <= weight_range.max_amount
+        ):
+            return True
+        else:
+            return False
