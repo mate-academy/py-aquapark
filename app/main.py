eacb8317161565f5d920d4dd0546e42f2745b4fd
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-07 18:44:53 +0300

    My solution

--- app/main.py
+++ app/main.py
@@ -2,24 +2,65 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.max_amount = max_amount
+        self.min_amount = min_amount
+
+    def __set_name__(self, owner: any, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: any, owner: any) -> any:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: any, value: any) -> any:
+        if value < self.min_amount or value > self.max_amount:
+            setattr(instance, self.protected_name, None)
+            return
+        if type(value) != int:
+            setattr(instance, self.protected_name, None)
+            return
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.height = height
+        self.weight = weight
+        self.age = age
+        self.name = name
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.height = height
+        self.weight = weight
+        self.age = age
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str,
+                 limitation_class: SlideLimitationValidator) -> None:
+        self.limitation_class = limitation_class
+        self.name = name
+
+    def can_access(self, visitor: Visitor) -> bool:
+        limitation = self.limitation_class(visitor.age,
+                                           visitor.weight,
+                                           visitor.height)
+        if getattr(limitation, "age") \
+                and getattr(limitation, "weight") \
+                and getattr(limitation, "height"):
+            return True
+        return False
