38c3a307a5113f67fafe63e8a0642c35e6e3c1d7
laskarj <koks.adekvat@gmail.com>
2023-06-27 20:22:21 +0200

    Fixed annotation.

--- app/main.py
+++ app/main.py
@@ -1,37 +1,5 @@
 from abc import ABC, abstractmethod
-
-
-class IntegerRange:
-
-    def __init__(self, min_amount: int, max_amount: int) -> None:
-        self.min_amount = min_amount
-        self.max_amount = max_amount
-        self.status_access = None
-
-    def __set_name__(self,
-                     owner: "SlideLimitationValidator",
-                     name: str) -> None:
-        self.status_access = "_" + name
-
-    def __get__(self,
-                instance: "SlideLimitationValidator",
-                owner: "SlideLimitationValidator"
-                ) -> bool:
-        return getattr(instance, self.status_access)
-
-    def __set__(self,
-                instance: "SlideLimitationValidator",
-                value: int) -> None:
-        if not isinstance(value, int):
-            raise TypeError(
-                f"{self.status_access} should be integer,"
-                f" not a {type(value)}!"
-            )
-        setattr(
-            instance,
-            self.status_access,
-            self.min_amount <= value <= self.max_amount
-        )
+from typing import Type
 
 
 class Visitor:
@@ -59,6 +27,39 @@ class SlideLimitationValidator(ABC):
     @abstractmethod
     def get_access(self) -> bool:
         pass
+
+
+class IntegerRange:
+
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+        self.status_access = None
+
+    def __set_name__(self,
+                     owner: Type[SlideLimitationValidator],
+                     name: str) -> None:
+        self.status_access = "_" + name
+
+    def __get__(self,
+                instance: Type[SlideLimitationValidator],
+                owner: Type[SlideLimitationValidator]
+                ) -> bool:
+        return getattr(instance, self.status_access)
+
+    def __set__(self,
+                instance: Type[SlideLimitationValidator],
+                value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError(
+                f"{self.status_access} should be integer,"
+                f" not a {type(value)}!"
+            )
+        setattr(
+            instance,
+            self.status_access,
+            self.min_amount <= value <= self.max_amount
+        )
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
@@ -96,12 +97,12 @@ class Slide:
     def __init__(
             self,
             name: str,
-            limitation_class: "SlideLimitationValidator"
+            limitation_class: Type[SlideLimitationValidator]
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
-    def can_access(self, visitor: "Visitor") -> bool:
+    def can_access(self, visitor: Type[Visitor]) -> bool:
         return self.limitation_class(
             age=visitor.age,
             weight=visitor.weight,
