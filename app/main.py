6df0710067a90039849f612418e524c74b946639
Serhii Berdianskyi <zeferot94@gmail.com>
2023-09-20 15:14:54 +0300

    changed brackets for type[SlideLimitationValidator]

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 class Slide:
     def __init__(self,
                  name: str,
-                 limitation_class: type(SlideLimitationValidator)
+                 limitation_class: type[SlideLimitationValidator]
                  ) -> None:
         self.name = name
         self.limitation_class = limitation_class
