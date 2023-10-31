6e3ffbe14984a40a61bdfb8b2128817448c0b848
Sergii Pryima <spryima@gmail.com>
2023-10-31 22:12:30 +0200

    annotations fixed

--- app/main.py
+++ app/main.py
@@ -48,7 +48,9 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 
 class Slide:
 
-    def __init__(self, name: str, limitation_class: Type[SlideLimitationValidator]) -> None:
+    def __init__(self,
+                 name: str,
+                 limitation_class: Type[SlideLimitationValidator]) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
