ef536d3938a2c7a977414dcf5112c1ee8237bb1d
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-17 16:01:02 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,78 +1,89 @@
-from abc import ABC
+rom abc import ABC
 
 
 class IntegerRange:
-    def __init__(self, min_value: int, max_value: int) -> None:
-        if min_value > max_value:
-            raise ValueError(
-                "Not in range"
-            )
 
-        self.min_value = min_value
-        self.max_value = max_value
+    def __init__(self,
+                 min_amount: int,
+                 max_amount: int) -> None:
 
-    def __get__(self, obj: any, owner: any) -> int:
-        return getattr(obj, self.__name__)
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, obj, name) -> None:
+        self._name = "_" + name
+
+    def __get__(self, obj, owner) -> int:
+        return getattr(obj, self._name)
 
     def __set__(self, obj: any, value: int) -> None:
 
-        if value < self.min_value or value > self.max_value:
+        if not self.min_amount <= value <= self.max_amount:
             raise ValueError("Not in range")
 
-        setattr(obj, self.__name__, value)
+        setattr(obj, self._name, value)
+
+
+class Visitor:
+
+    def __init(self,
+               name: str,
+               age: int,
+               weight: int,
+               height: int) -> None:
+
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    def __init__(self, age_range: IntegerRange,
-                 weight_range: IntegerRange,
-                 height_range: IntegerRange
-                 ) -> None:
-        self.age_range = age_range
-        self.weight_range = weight_range
-        self.height_range = height_range
 
-    def __set__(self, obj: any, value: any) -> None:
-        setattr(obj, "__dict__", value)
+    def __int__(self,
+                age_integer: IntegerRange,
+                weight_integer: IntegerRange,
+                height_integer: IntegerRange
+                ) -> None:
+
+        self.age = age_integer
+        self.weight = weight_integer
+        self.height = height_integer
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
     def __init__(self) -> None:
-        super().__init__(age_range=IntegerRange(4, 14),
-                         weight_range=IntegerRange(20, 50),
-                         height_range=IntegerRange(80, 120))
+        super().__init__(age_integer=IntegerRange(4, 14),
+                         weight_integer=IntegerRange(20, 50),
+                         height_integer=IntegerRange(80, 120))
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
     def __init__(self) -> None:
-        super().__init__(age_range=IntegerRange(14, 60),
-                         weight_range=IntegerRange(50, 120),
-                         height_range=IntegerRange(120, 200))
-
-
-class Visitor:
-    def __init__(self,
-                 name: str,
-                 age: int,
-                 height: int,
-                 weight: int) -> None:
-        self.name = name
-        self.age = age
-        self.height = height
-        self.weight = weight
+        super().__init__(age_integer=IntegerRange(14, 60),
+                         weight_integer=IntegerRange(50, 120),
+                         height_integer=IntegerRange(120, 220))
 
 
 class Slide:
+
     def __init__(self,
                  name: str,
                  limitation_class: SlideLimitationValidator) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
-    def can_access(self, visitor: any) -> bool:
+    def can_access(self, visitor: Visitor) -> bool:
+
         try:
-            self.limitation_class = visitor.age
-            self.limitation_class = visitor.weight
-            self.limitation_class = visitor.height
+            # self.limitation_class = visitor.age
+            # self.limitation_class = visitor.weight
+            # self.limitation_class = visitor.height
+
+            visitor.age = self.limitation_class._age
+            visitor.weight = self.limitation_class._weight
+            visitor.height = self.limitation_class._height
+        
         except ValueError:
             return False
 
