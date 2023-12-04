0b3e1a1f8c8d83bcf75004d8334f93dd12a43260
Vitalii <ananievvitalii10@gmail.com>
2023-12-04 22:47:23 +0200

    Aqua-park

--- app/main.py
+++ app/main.py
@@ -1,25 +1,75 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Union
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.name = name
+
+    def __get__(self, instance: object, owner: type) -> Union:
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance: object, value: int) -> None:
+        if not self.min_amount <= value <= self.max_amount:
+            raise ValueError(
+                f"{self.name} must be in the range"
+                f" {self.min_amount}-{self.max_amount}")
+        instance.__dict__[self.name] = value
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: tuple, weight: tuple, height: tuple) -> None:
+        self.age = IntegerRange(*age)
+        self.weight = IntegerRange(*weight)
+        self.height = IntegerRange(*height)
+
+    @abstractmethod
+    def validate(self, visitor: Visitor) -> bool:
+        pass
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    def __init__(self) -> None:
+        super().__init__((4, 14), (20, 50), (80, 120))
+
+    def validate(self, visitor: Visitor) -> bool:
+        return (self.age.min_amount <= visitor.age <= self.age.max_amount
+                and self.weight.min_amount <= visitor.weight
+                <= self.weight.max_amount
+                and self.height.min_amount <= visitor.height
+                <= self.height.max_amount
+                )
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    def __init__(self) -> None:
+        super().__init__((14, 60), (50, 120), (120, 220))
+
+    def validate(self, visitor: Visitor) -> bool:
+        return (self.age.min_amount <= visitor.age <= self.age.max_amount
+                and self.weight.min_amount <= visitor.weight
+                <= self.weight.max_amount
+                and self.height.min_amount <= visitor.height
+                <= self.height.max_amount
+                )
 
 
 class Slide:
-    pass
+    def __init__(self, name: str, limitation_class: type) -> None:
+        self.name = name
+        self.limitation_validator = limitation_class()
+
+    def can_access(self, visitor: Visitor) -> bool:
+        return self.limitation_validator.validate(visitor)
