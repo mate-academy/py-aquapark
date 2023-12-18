98bcf77d64fb5e957131c986b80a9bc665ac6b73
Veronika <veronikaboeva935@gmail.com>
2023-12-18 20:21:37 +0100

    Solution 18.12

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ class IntegerRange:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
-    def __set_name__(self, owner: object, name: str) -> None:
+    def __set_name__(self, owner: type, name: str) -> None:
         self.public_name = name
         self.private_name = "_" + name
 
-    def __get__(self, instance: str, owner: type) -> None:
+    def __get__(self, instance: object, owner: type) -> None:
         return getattr(instance, self.private_name)
 
-    def __set__(self, instance: str, value: int) -> None:
+    def __set__(self, instance: object, value: int) -> None:
         if not (self.min_amount <= value <= self.max_amount):
             raise ValueError(f"Value should be within range: "
                              f"{self.min_amount} - {self.max_amount}.")
