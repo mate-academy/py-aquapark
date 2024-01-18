d1c71e09792ff456fda3ef4c6a43f1c691a84272
Maksym Nimych <maksimnimitch@gmail.com>
2024-01-18 19:19:03 +0200

    Fix annotation

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ class Slide:
     def __init__(
             self,
             name: str,
-            limitation_class: SlideLimitationValidator
+            limitation_class: type[SlideLimitationValidator]
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
