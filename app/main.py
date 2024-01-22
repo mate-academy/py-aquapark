5f3f63b0d9eeeac41d31f22cf611bc8c042727a1
sergey <38cerg12021979@jmal.com>
2024-01-22 15:50:57 +0200

    22_01_24

--- app/main.py
+++ app/main.py
@@ -2,24 +2,64 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: object, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: object, owner: object) -> int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value: int):
+        if not instance(value, int):
+            raise TypeError(f"{self.protected_name} should be an integer.")
+        if not self.min_amount <= value <= self.max_amount:
+            raise ValueError(f"{self.protected_name} should not be less than {self.min_amount} "
+                             f"and greater than {self.max_amount} ")
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str, limitation_class) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor) -> bool:
+
+        try:
+            self.limitation_class(age=visitor.age,
+                                  height=visitor.height,
+                                  weight=visitor.weight)
+            return True
+        except (ValueError, TypeError):
+            return False
+
