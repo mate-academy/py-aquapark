6d5a2afee3107e11671fff49bce251f06b294487
Mykola Kokoiev <veokoknik@gmail.com>
2024-01-25 19:21:35 +0100

    SOL

--- app/main.py
+++ app/main.py
@@ -1,25 +1,79 @@
+from __future__ import annotations
 from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: [ABC], name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: ABC, owner: [ABC]) -> int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: ABC, value: int) -> None:
+        if self.min_amount <= value <= self.max_amount:
+            setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(
+            self,
+            name: str,
+            age: int,
+            height: int,
+            weight: int,
+    ) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(
+            self,
+            age: int,
+            height: int,
+            weight: int
+    ) -> None:
+        self.age = age
+        self.height = height
+        self.weight = weight
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(
+            self,
+            name: str,
+            limitation_class: [SlideLimitationValidator]
+    ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        access = self.limitation_class(
+            visitor.age,
+            visitor.height,
+            visitor.weight
+        )
+
+        return all(
+            [hasattr(access, "age"),
+             hasattr(access, "height"),
+             hasattr(access, "weight")]
+        )
