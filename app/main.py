5e9d800e1a54a1cc45a278b4a4bc295b1c0fd5b1
Mikola Rozhansky <bymikola@gmail.com>
2024-03-16 21:17:25 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,25 +1,64 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: object, name) -> None:
+        self.prіvate_name = "_" + name
+
+    def __get__(self, instance: object, owner: object) -> None:
+        return getattr(instance, self.prіvate_name)
+
+    def __set__(self, instance: object, value) -> None:
+        if not (self.min_amount <= value <= self.max_amount):
+            return setattr(instance, self.prіvate_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        super().__init__(age, height, weight)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        super().__init__(age, height, weight)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str, limitation_class: SlideLimitationValidator) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, inst: Visitor) -> None:
+        print(f"Visitor age: {inst.age} class limitation: {self.limitation_class.age})")
+
+
+
+
